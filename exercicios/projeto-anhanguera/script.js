131fe6e6db2d83771c474abf057f4d8a5fc6e6be
ewcaio <caiosilvanovieira@gmail.com>
2023-05-25 15:52:01 -0300

    atualizando dados

---
+++ exercicios/projeto-anhanguera/script.js
@@ -0,0 +1,6 @@
+let sidebar = document.querySelector(".sidebar")
+let sidebarBtn = document.querySelector(".sidebar")
+
+sidebarBtn.onclick = function() {
+    sidebar.classList.toggle("active");
+}
